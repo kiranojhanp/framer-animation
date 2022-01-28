ba8e953acf570461207e26d972de91d028e892ff
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2022-01-28 18:17:35 +0545

    init framer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {motion} from "framer-motion"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className="App">
+			<header className="App-header">
+				<motion.img
+					src={logo}
+					animate={{ rotate: 360 }}
+					transition={{
+						duration: 7,
+						ease: "linear",
+						times: [0, 0.2, 0.5, 0.8, 1],
+						loop: Infinity,
+					}}
+					className="App-logo"
+					alt="logo"
+				/>
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Framer/Framer.stories.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Framer from "./Framer";
+
+export default {
+	title: "Example/Framer",
+	component: Framer,
+	argTypes: {
+		backgroundColor: { control: "color" },
+	},
+};
+
+const Template = (args) => <Framer {...args} />;
+
+export const AnimatedDiv = Template.bind({});
+AnimatedDiv.args = {};
